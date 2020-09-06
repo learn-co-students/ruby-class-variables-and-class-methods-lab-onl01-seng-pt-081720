27099a871ec9cdab64922557f430b5a020ca43ab
Jason Markson <jasonmarkson76@gmail.com>
2020-09-06 20:33:01 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+require 'pry'
+
+class Song 
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  attr_accessor :name, :artist, :genre 
+
+
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @@artists << artist
+    @@genres << genre
+    @name = name
+    @artist = artist 
+    @genre = genre
+
+  end
+
+
+  def self.count
+    @@count
+  end
+  
+  def self.artists 
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    counts = Hash.new(0)
+    @@genres.each { |name| counts[name] += 1 }
+    counts
+  end
+  
+  def self.artist_count
+    counts = Hash.new(0)
+    @@artists.each { |name| counts[name] += 1 }
+    counts
+  end
+end
