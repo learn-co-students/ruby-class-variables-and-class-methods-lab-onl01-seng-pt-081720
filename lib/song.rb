021c407bb33817b525edaf76d388470ff75b862c
Tim Flood <tflood9@gmail.com>
2020-09-07 12:47:28 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  def self.genres
+    @@genres.uniq!
+  end
+
+  def self.genre_count
+    @@genre_count = {}
+
+    @@genres.group_by(&:itself).each {|k,v| @@genre_count[k] = v.count}
+
+    return @@genre_count
+  end
+
+  def self.artists
+    @@artists.uniq!
+  end
+
+  def self.artist_count
+    @@artist_count
+
+    @@artists.group_by(&:itself).each {|k,v| @@artist_count[k] = v.count}
+
+    return @@artist_count
+  end
+
+  def self.count
+    @@count
+  end
+end
