5458cbd1228ee024019448e471188e0bdc5d68a6
Jocelyn  Ramirez <ramirez.jocelyn73@yahoo.com>
2020-09-09 01:42:04 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,48 @@
+  require 'pry'
+class Song 
+  attr_reader :name, :artist, :genre
+  @@count= 0
+  @@artists =[]
+  @@genres=[]
+  def initialize (name, artist, genre)
+         @name = name
+         @artist= artist
+         @genre = genre
+         @@count += 1 
+         @@artists << artist
+         @@genres << genre
+  end
+ 
+ def self.count
+   @@count
+ end
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+  def self.genre_count 
+    hash={}
+    @@genres.each do |k|
+      if hash[k]
+        hash[k] += 1
+      else
+        hash[k]= 1
+      end
+   
+    end
+         hash
+  end
+  def self.artist_count
+    artist_hash={}
+    @@artists.each do |k|
+      if artist_hash[k] 
+        artist_hash [k] += 1 
+      else 
+        artist_hash[k] = 1 
+      end
+  end
+  artist_hash
+  end
+end
